c7c8f3b8588005082a45a988697ca5c8d916872f
ftsguf <ocae.lormois@gmail.com>
2021-11-16 13:21:28 +0100

    built-in cd perfect

--- built_in/ft_built_in_cd.c
+++ built_in/ft_built_in_cd.c
@@ -6,18 +6,57 @@
 /*   By: lchristo <lchristo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 21:52:54 by lchristo          #+#    #+#             */
-/*   Updated: 2021/11/16 12:42:17 by lchristo         ###   ########.fr       */
+/*   Updated: 2021/11/16 13:17:08 by lchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int		ft_equal(char *str)
+{
+	int		i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == '=')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_get_home(void)
+{
+	t_env *cpy;
+	int		len;
+
+	len = ft_get_lenkey("HOME=");
+	cpy = *get_adress_env();
+	while (cpy)
+	{
+		if (len == ft_get_lenkey(cpy->str) && !ft_truestrncmp(cpy->str, "HOME", len))
+		{
+			if (ft_equal(cpy->str))
+				return (cpy->str + len + 1);
+			return (NULL);
+		}
+		cpy = cpy->next;
+	}
+	return (NULL);
+}
+
 int    ft_built_in_cd(char **str)
 {
     if (*(str + 1) == NULL)
 	{
-		*(str + 1) = ft_get_value_of_env(get_adress_env(), "HOME");
-		if (*(str + 1) == NULL || str[1][0] == '\0')
+		*(str + 1) = ft_get_home();
+		if (*(str + 1) == NULL)
+		{
+			printf("HOME not set\n");
+			return (0);
+		}
+		if (str[1][0] == '\0')
 			return (0);
 	}
 	if (chdir(*(str + 1)) == -1)
