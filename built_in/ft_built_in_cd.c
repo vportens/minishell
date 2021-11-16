44cff6d22c400a7dde79bc398f532d0b271fbe15
ftsguf <ocae.lormois@gmail.com>
2021-11-16 12:43:07 +0100

    built-in cd corriger

--- built_in/ft_built_in_cd.c
+++ built_in/ft_built_in_cd.c
@@ -6,7 +6,7 @@
 /*   By: lchristo <lchristo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 21:52:54 by lchristo          #+#    #+#             */
-/*   Updated: 2021/11/04 10:11:03 by lchristo         ###   ########.fr       */
+/*   Updated: 2021/11/16 12:42:17 by lchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 int    ft_built_in_cd(char **str)
 {
     if (*(str + 1) == NULL)
-        return 0;
+	{
+		*(str + 1) = ft_get_value_of_env(get_adress_env(), "HOME");
+		if (*(str + 1) == NULL || str[1][0] == '\0')
+			return (0);
+	}
 	if (chdir(*(str + 1)) == -1)
         if (ft_print_error("minishell: cd: ", *(str + 1)) == 50)
             return (50);
